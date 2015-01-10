a0e159c3a9bbf93d627ec33b52e57068e2f938cc
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2015-01-10 17:34:02 -0430

    Refactoring en crear encuesta command

--- protected/commands/CrearEncuestaCommand.php
+++ protected/commands/CrearEncuestaCommand.php
@@ -6,6 +6,10 @@
  */
 class CrearEncuestaCommand extends CConsoleCommand
 {
+    public $nombres = array(
+        "Encuesta de Universidades",
+        "Encuesta para laboratorios, centros y otros",
+    );
     public function run($args)
     {
         $tipoencuesta = Tipoencuesta::model()
@@ -19,13 +23,15 @@ class CrearEncuestaCommand extends CConsoleCommand
         ));
         $sqlInsert = <<<EOF
 INSERT INTO {{Encuesta}} (tipoencuesta_id ,  enunciado ,  identificador ,  fecha_inicial ,  fecha_final ,  creado_en ,  actualizado_en ,  user_id ,  ano, activa ) VALUES
-(:tipoencuesta_id, 'Test', 'test_encuesta_comm', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2014-12-17 21:17:11', '0000-00-00 00:00:00', :userid, 2014, 1);
+(:tipoencuesta_id, :enun, :iden, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2014-12-17 21:17:11', '0000-00-00 00:00:00', :userid, 2014, 1);
 EOF;
         $commandEncuesta = Yii::app()->db->createCommand($sqlInsert);
-        foreach ($tipoencuesta as $te) {
+        foreach ($tipoencuesta as $i=>$te) {
             $filasAfectadas = $commandEncuesta->execute(array(
                 ':userid' => $userid,
                 ':tipoencuesta_id' => $te->id,
+                ':enun' => $this->nombres[$i%count($tipoencuesta)],
+                ':iden' => $i,
             ));
             echo "Comando de insertar encuesta afectó $filasAfectadas";
         }
